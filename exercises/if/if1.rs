839bd5f9b6acd234367e7b10703e62918c68d450
Jacek Chmielewski <jchmielewski@teonite.com>
2023-01-30 08:27:58 +0100

    Done up to if2

--- exercises/if/if1.rs
+++ exercises/if/if1.rs
@@ -1,14 +1,19 @@
+use std::cmp::max as bigger;
 // if1.rs
 // Execute `rustlings hint if1` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
-pub fn bigger(a: i32, b: i32) -> i32 {
-    // Complete this function to return the bigger number!
-    // Do not use:
-    // - another function call
-    // - additional variables
-}
+// pub fn bigger(a: i32, b: i32) -> i32 {
+//     // Complete this function to return the bigger number!
+//     // Do not use:
+//     // - another function call
+//     // - additional variables
+//     std::cmp::max(a, b)
+//     // if a > b {
+//     //     a
+//     // } else {
+//     //     b
+//     // }
+// }
 
 // Don't mind this for now :)
 #[cfg(test)]
