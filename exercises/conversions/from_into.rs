fdcc30ca19fcee5b1655fd41f704dafa212fd6fc
Jacek Chmielewski <jchmielewski@teonite.com>
2023-02-12 09:00:44 +0100

    All done

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -35,10 +35,23 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of Person
 // Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
-    fn from(s: &str) -> Person {
+    fn from(s: &str) -> Self {
+        let values: Vec<&str> = s.split(',').collect();
+        if values.len() != 2 {
+            return Self::default();
+        }
+        let (name, age) = (values[0].to_string(), values[1]);
+        if name.len() == 0 {
+            return Self::default();
+        }
+        let age = if let Ok(age) = age.parse::<usize>() {
+            age
+        } else {
+            return Self::default();
+        };
+
+        Person { name, age }
     }
 }
 
